ecaa38c912958ccbdb71893a445674536cd6fd93
ZYK <zhangyongkang2009@gmail.com>
2024-12-08 22:26:41 +0800

    debug

--- i18n.config.ts
+++ i18n.config.ts
@@ -4,6 +4,7 @@ export default {
     missingWarn: false,  // 禁用缺失警告
     fallbackWarn: false, // 禁用回退警告
     warnHtmlMessage: false, // 禁用 HTML 消息警告
+    inheritLocale: false,// 添加这个配置来处理未找到翻译的情况
     localeCodes: {
         'en': 'en-US',
         'zh': 'zh-CN',
@@ -19,5 +20,9 @@ export default {
         'tr': 'tr',
         'uk': 'uk-UA',
         'zh-TW': 'zh-TW'
-    }
+    },
+    missing: (locale: string, key: string) => {
+        console.warn(`Missing translation: ${locale} ${key}`)
+        return key
+      }
 }
